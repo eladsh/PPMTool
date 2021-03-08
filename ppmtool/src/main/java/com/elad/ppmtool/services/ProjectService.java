a21a5ad5182f2a629433637e29449bb18177e44f
Elad_Shaked <e8823s@gmail.com>
2021-03-08 17:38:58 +0200

    Find Project by Identifier

--- ppmtool/src/main/java/com/elad/ppmtool/services/ProjectService.java
+++ ppmtool/src/main/java/com/elad/ppmtool/services/ProjectService.java
@@ -21,8 +21,16 @@ public class ProjectService {
         } catch (Exception e) {
             throw new ProjectIdException("Project ID '" + project.getProjectIdentifier().toUpperCase() + "' already exist");
         }
+    }
 
+    public Project findProjectByIdentifier(String projectId){
+        Project project =  projectRepository.findByProjectIdentifier(projectId.toUpperCase());
 
+        if(project == null){
+            throw new ProjectIdException("Project ID '" + projectId + "' does not exist");
+        }
+
+        return project;
     }
 
 }                       
