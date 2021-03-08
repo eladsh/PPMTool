78b1a4968e06d6b9e18698ea0c10a461521edc11
Elad_Shaked <e8823s@gmail.com>
2021-03-08 07:55:32 +0200

    Project Repository & adding H2 properties

--- ppmtool/src/main/java/com/elad/ppmtool/domain/Project.java
+++ ppmtool/src/main/java/com/elad/ppmtool/domain/Project.java
@@ -1,4 +1,39 @@
 package com.elad.ppmtool.domain;
 
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
 public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String projectName;
+    private String projectIdentifier;
+    private String description;
+    private Date start_date;
+    private Date end_date;
+
+    private Date created_At;
+    private Date updated_At;
+
+    @PrePersist
+    protected void onCreate(){
+        this.created_At = new Date();
+    }
+
+    @PreUpdate
+    protected void onUpdate(){
+        this.updated_At = new Date();
+    }
+
 }
---
+++ ppmtool/src/main/java/com/elad/ppmtool/repositories/ProjectRepository.java
@@ -0,0 +1,12 @@
+package com.elad.ppmtool.repositories;
+
+import com.elad.ppmtool.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+
+    @Override
+    Iterable<Project> findAllById(Iterable<Long> iterable);
+}
